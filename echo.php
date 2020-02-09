0d991bcd06ad4b06147799c54064ae779ea13ed1
shalomcarmel <shalom@globaldots.com>
2020-02-09 10:40:35 +0200

    add akamai letsencrypt support

--- echo.php
+++ echo.php
@@ -15,8 +15,8 @@
   *
 **/
 
-$version = "1.9.5";
-$author = 'Shalom Carmel 2016';
+$version = "2.0.1";
+$author = 'Shalom Carmel 2016/2018';
 
 require_once('./lib/GeoIP/GeoIP.php');
 
@@ -42,6 +42,36 @@ if (!defined("JSON_UNESCAPED_SLASHES")) {
  // Allow usage with AJAX 
  header('Access-Control-Allow-Origin: *'); 
 
+ 
+ 
+// Add LetsEncrypt automatic redirection. 
+// This is the first thing to do. 
+
+$isLetsEncryptValidation = false; 
+$uri = $_SERVER["REQUEST_URI"] ;
+$path = parse_url($uri, PHP_URL_PATH) ;
+
+$pos = strpos($path, ".well-known/acme-challenge");
+
+// Note our use of ===.  Simply == would not work as expected
+// because the position of 'a' was the 0th (first) character.
+if ($pos === false) {
+    // do nothing, this is not a LetsEncrypt redirection request
+} else {
+    $akamaiDCV = "http://dcv.akamai.com" . $uri;
+	http_response_code(302);
+    header('Location: ' . $akamaiDCV); 
+	exit();
+}
+
+// example source: http://collect-direct.danidin.net/.well-known/acme-challenge/1MQv0IrrdvUfM_CFQOntEhcfzlo-EyYnhIJtG1EPgQk
+// example result:             http://dcv.akamai.com/.well-known/acme-challenge/1MQv0IrrdvUfM_CFQOntEhcfzlo-EyYnhIJtG1EPgQk
+
+
+
+
+
+ 
 // Output json by default, output text only if instructed to by an appropriate header or a query string item. 
 if ( empty($_SERVER['HTTP_X_ECHO_TYPE']) && empty($_GET['x-echo-type'] ) && empty($configuration_array["response-type"])) {
 	$json=true; 
@@ -204,6 +234,70 @@ if( !function_exists('apache_request_headers') ) {
     }
 }
 
+if (!function_exists('http_response_code')) {
+	function http_response_code($code = NULL) {
+
+		if ($code !== NULL) {
+
+			switch ($code) {
+				case 100: $text = 'Continue'; break;
+				case 101: $text = 'Switching Protocols'; break;
+				case 200: $text = 'OK'; break;
+				case 201: $text = 'Created'; break;
+				case 202: $text = 'Accepted'; break;
+				case 203: $text = 'Non-Authoritative Information'; break;
+				case 204: $text = 'No Content'; break;
+				case 205: $text = 'Reset Content'; break;
+				case 206: $text = 'Partial Content'; break;
+				case 300: $text = 'Multiple Choices'; break;
+				case 301: $text = 'Moved Permanently'; break;
+				case 302: $text = 'Moved Temporarily'; break;
+				case 303: $text = 'See Other'; break;
+				case 304: $text = 'Not Modified'; break;
+				case 305: $text = 'Use Proxy'; break;
+				case 400: $text = 'Bad Request'; break;
+				case 401: $text = 'Unauthorized'; break;
+				case 402: $text = 'Payment Required'; break;
+				case 403: $text = 'Forbidden'; break;
+				case 404: $text = 'Not Found'; break;
+				case 405: $text = 'Method Not Allowed'; break;
+				case 406: $text = 'Not Acceptable'; break;
+				case 407: $text = 'Proxy Authentication Required'; break;
+				case 408: $text = 'Request Time-out'; break;
+				case 409: $text = 'Conflict'; break;
+				case 410: $text = 'Gone'; break;
+				case 411: $text = 'Length Required'; break;
+				case 412: $text = 'Precondition Failed'; break;
+				case 413: $text = 'Request Entity Too Large'; break;
+				case 414: $text = 'Request-URI Too Large'; break;
+				case 415: $text = 'Unsupported Media Type'; break;
+				case 500: $text = 'Internal Server Error'; break;
+				case 501: $text = 'Not Implemented'; break;
+				case 502: $text = 'Bad Gateway'; break;
+				case 503: $text = 'Service Unavailable'; break;
+				case 504: $text = 'Gateway Time-out'; break;
+				case 505: $text = 'HTTP Version not supported'; break;
+				default:
+					exit('Unknown http status code "' . htmlentities($code) . '"');
+				break;
+			}
+
+			$protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
+
+			header($protocol . ' ' . $code . ' ' . $text);
+
+			$GLOBALS['http_response_code'] = $code;
+
+		} else {
+
+			$code = (isset($GLOBALS['http_response_code']) ? $GLOBALS['http_response_code'] : 200);
+
+		}
+
+		return $code;
+
+	}
+}
 
 
 ?>
